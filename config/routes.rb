599d8029e4e4324ab659030a175db773b0f3dd01
mgmilton <matt.milton@gmail.com>
2018-04-30 15:58:05 -0600

    adds food#show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :foods do
         get '/', :to => "foods#index"
+        get '/:id', :to => "foods#show"
       end
     end
   end
