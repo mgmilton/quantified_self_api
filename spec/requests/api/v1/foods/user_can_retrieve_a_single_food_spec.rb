61641e8dbe7069f50bc8fea85630b991a83fd56c
mgmilton <matt.milton@gmail.com>
2018-04-30 15:58:26 -0600

    adds retrieval of single food object test

---
+++ spec/requests/api/v1/foods/user_can_retrieve_a_single_food_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+context 'get to api/v1/foods/:id' do
+  it "returns a matching food object" do
+    blood_sausage = create(:food, id: 10, name: 'morcilla')
+
+    get "/api/v1/foods/10"
+
+    food = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(food["name"]).to eq("morcilla")
+    expect(food["calories"]).to eq(400)
+  end
+
+  it "returns a 404 if food not found" do
+    get '/api/v1/foods/303003'
+
+    error = JSON.parse(response.body)
+
+    expect(response.status).to eq(404)
+    expect(error).to eq({"error"=>"not-found"})
+  end
+end
