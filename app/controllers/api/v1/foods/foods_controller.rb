0d29323508d6dd184504bfce59bc2c3a6939d76a
mgmilton <matt.milton@gmail.com>
2018-04-30 15:57:51 -0600

    adds set food private methods

--- app/controllers/api/v1/foods/foods_controller.rb
+++ app/controllers/api/v1/foods/foods_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::Foods::FoodsController < ApplicationController
+  before_action :set_food, :nil_food?, only: [:show]
 
   def index
     render json: Food.all
   end
 
+  def show
+    render json: Food.find(params[:id])
+  end
 
+  private
+    def set_food
+      @food = Food.find_by(id: params[:id])
+    end
+
+    def nil_food?
+      if @food.nil?
+        render :json => {:error => "not-found"}.to_json, :status => 404
+      else
+        render json: @food
+      end
+    end
 end
