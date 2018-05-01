697234fcbcf62fb1bb31e69c9bfb1fff580a153f
mgmilton <matt.milton@gmail.com>
2018-05-01 14:33:40 -0600

    adds food creation method

--- app/controllers/api/v1/meals/meals_controller.rb
+++ app/controllers/api/v1/meals/meals_controller.rb
@@ -8,6 +8,10 @@ class Api::V1::Meals::MealsController < ApplicationController
     nil_meal?
   end
 
+  def create
+    food_storer
+  end
+
   private
     def set_meal
       @meal = Meal.find_by(id: params[:id])
@@ -18,6 +22,17 @@ class Api::V1::Meals::MealsController < ApplicationController
         render :json => {:error => "not-found"}.to_json, :status => 404
       else
         render json: @meal
+      end
+    end
+
+    def food_storer
+      @food = Food.find_by(id: params[:id])
+      @meal = Meal.find_by(id: params[:meal_id])
+      if @meal.nil? || @food.nil?
+        render :json => {:error => "not-found"}.to_json, :status => 404
+      else
+        @meal.mealfoods.create(food_id: @food.id)
+        render :json => {:message => "Successfully added #{@food.name} to #{@meal.name}"}.to_json, :status => 201
       end
     end
 end
